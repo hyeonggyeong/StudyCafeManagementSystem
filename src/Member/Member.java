65967a51f230d336d84c8a1536dd730f6b952030
hyeonggyeong <feels1010@naver.com>
2021-04-13 02:45:31 +0900

    MemberManager #5

---
+++ src/Member/Member.java
@@ -0,0 +1,82 @@
+package Member;
+
+
+import java.util.Scanner;
+
+public class Member {
+	protected MemberKind kind;
+	protected int id;
+	protected String name;
+	protected int time;
+	protected int locker;
+	
+	public Member() {	
+	}
+	
+	public Member(int id, String name, int time, int locker) {
+		this.id = id;
+		this.name = name;
+		this.time = time;
+		this.locker = locker;
+	}
+	public MemberKind getKind() {
+		return kind;
+	}
+
+	public void setKind(MemberKind kind) {
+		this.kind = kind;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public int getLocker() {
+		return locker;
+	}
+
+	public void setLocker(int locker) {
+		this.locker = locker;
+	}
+	
+	public void printInfo() {
+		System.out.print("ID : " + id);
+		System.out.print(" / Name : " + name);
+		System.out.print(" / Timeticket : " + time);
+		System.out.println(" / Locker Number: " + locker);
+	}
+	public void getUserInput(Scanner input) {
+		System.out.print("Member ID(PhoneNumber without '-') :");
+		int id =input.nextInt();
+		this.setId(id);
+		System.out.print("Member Name :");
+		String name = input.next();
+		this.setName(name);
+		System.out.print("Desired Timeticket[Hour] :");
+		int time = input.nextInt();
+		this.setTime(time);
+		System.out.print("Locker Number :");
+		int locker = input.nextInt();
+		this.setLocker(locker);
+	}
+}
--- src/MemberManager.java
+++ src/MemberManager.java
@@ -1,39 +1,66 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import Member.Member;
+import Member.MenMember;
+import Member.WomenMember;
+
 public class MemberManager {
-	Member member;
+	ArrayList<Member> members= new ArrayList<Member>();
 	Scanner input;	
 	MemberManager(Scanner input) {
 		this.input = input;
 	}
 	
 	public void addmembers() {
-		System.out.println("****************ADD******************");
-		member = new Member();
-		System.out.print("Member ID(PhoneNumber without '-') :");
-		member.id =input.nextInt();
-		System.out.print("Member Name :");
-		member.name = input.next();
-		System.out.print("Desired Timeticket[Hour] :");
-		member.time = input.nextInt();
-		System.out.print("Locker Number :");
-		member.locker = input.nextInt();
-		
+		int kind = 0;
+		Member member;
+		while (kind != 1 && kind !=2) {
+			System.out.println("****************ADD******************");
+			System.out.println("Select Gender: ");
+			System.out.println("1. Men");
+			System.out.println("2. Women");
+			kind = input.nextInt();
+			if (kind == 1) {
+				member = new MenMember();
+				member.getUserInput(input);
+				members.add(member);
+				break;
+			}
+			else if (kind ==2) {
+				member = new WomenMember();
+				member.getUserInput(input);
+				members.add(member);
+				break;
+			}
+			else {
+				System.out.println("Select num for Member's Genger between 1 and 2");
+			}
+		}		
 	}
 	public void deletemembers() {
 		System.out.println("***************DELETE******************");
 		System.out.print("Enter the MemberID you want to delete :");
 		int memberID =input.nextInt();
+		int index = -1;
 		
-		if(member == null) {
+		for (int i = 0; i<members.size(); i++) {
+			if(members.get(i).getId() == memberID) {
+				index = i;
+				break;		
+			}
+		}
+		if (index >= 0) {
+			members.remove(index);
+			System.out.println("Member " + memberID  +" is deleted");
+		}
+		
+		
+		else {
 			System.out.println("Member has not been registered");
 			return;
 		}
-		if(member.id == memberID) {
-			member = null;
-			System.out.println("Member is deleted");
-			
-		}
+		
 		
 		
 	}
@@ -41,47 +68,50 @@ public class MemberManager {
 		System.out.println("****************EDIT*******************");
 		System.out.print("Enter the MembertID you want to edit :");
 		int memberID =input.nextInt();
-		if(member == null) {
-			System.out.println("Member has not been registered");
-			return;
-		}
-		if (member.id == memberID) {
-			System.out.println("Enter the number you want to edit");
-			System.out.println("1. ID");
-			System.out.println("2. Name");
-			System.out.println("3. Time Ticket");
-			System.out.println("4. Locker Number");
-			 int editnum = input.nextInt();
-			if (editnum == 1) {
-				System.out.print("Member ID(PhoneNumber without '-') :");
-				member.id =input.nextInt();
-			}
-			else if (editnum == 2) {
-				System.out.print("Member Name :");
-				member.name = input.next();
-			}
-			else if (editnum == 3) {
-				System.out.print("Desired Timeticket[Hour] :");
-				member.time = input.nextInt();
-			}
-			else if (editnum == 4) {
-				System.out.print("Locker Number :");
-				member.locker = input.nextInt();
-			}
-		}
+		for (int i = 0; i<members.size(); i++) {
+			Member member = members.get(i);
+			if(member.getId() == memberID) {
+				int editnum = -1;				
+				while (editnum != 5) {
+					System.out.println("Enter the number you want to edit");
+					System.out.println("1. ID");
+					System.out.println("2. Name");
+					System.out.println("3. Time Ticket");
+					System.out.println("4. Locker Number");
+					System.out.println("5. Exit");
+					editnum = input.nextInt();
+					if (editnum == 1) {
+						System.out.print("Member ID(PhoneNumber without '-') :");
+						int id =input.nextInt();
+						member.setId(id);
+					}
+					else if (editnum == 2) {
+						System.out.print("Member Name :");
+						String name = input.next();
+						member.setName(name);
+					}
+					else if (editnum == 3) {
+						System.out.print("Desired Timeticket[Hour] :");
+						int time = input.nextInt();
+						member.setTime(time);
+					}
+					else if (editnum == 4) {
+						System.out.print("Locker Number :");
+						int locker = input.nextInt();
+						member.setLocker(locker);
+					}
+					else {
+						continue;	
+					}
+				}
+				break;
+			}						
+		}		
 	}
 	public void viewmembers() {
-		System.out.println("****************VIEW*******************");
-		System.out.print("Enter the MemberID you want to view :");
-		int memberID =input.nextInt();
-		
-		if(member == null) {
-			System.out.println("Member has not been registered");
-			return;
-		}
-		
-		if (member.id == memberID) {
-			member.printInfo();
+		System.out.println("# of registered members : " + members.size());
+		for (int i = 0; i<members.size(); i++) {
+			members.get(i).printInfo();
 		}
 	}
 }
