9cbc8b42a8c33671b00535410a64d90838c564be
hyeonggyeong <feels1010@naver.com>
2021-04-13 02:47:26 +0900

    WomenMember #5

---
+++ src/Member/WomenMember.java
@@ -0,0 +1,38 @@
+package Member;
+
+import java.util.Scanner;
+
+public class WomenMember extends Member {
+	public void getUserInput(Scanner input) {
+		System.out.print("Member ID(PhoneNumber without '-') :");
+		int id =input.nextInt();
+		this.setId(id);
+		
+		System.out.print("Member Name :");
+		String name = input.next();
+		this.setName(name);
+		
+		System.out.print("Desired Timeticket[Hour] :");
+		int time = input.nextInt();
+		this.setTime(time);
+		
+		char answer = 'x';
+		while (answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N') {
+			System.out.print("Do you want to use locker? (Y/N)");
+			answer = input.next().charAt(0);
+			if (answer == 'y'|| answer =='Y') {
+				System.out.print("Locker Number(Women : 101~200) :");
+				int locker = input.nextInt();
+				this.setLocker(locker);	
+				break;
+			}
+			else if (answer == 'n'|| answer =='N') {
+				this.setLocker(0);
+				break;
+			}
+			else {
+				
+			}	
+		}		
+	}
+}
