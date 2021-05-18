00d0edba2b60af8c53d14e9ee85848bf0413bd4a
hyeonggyeong <feels1010@naver.com>
2021-05-18 11:30:54 +0900

    MenuManager #8

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,41 +1,57 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		MemberManager membermanager = new MemberManager(input);
-		int num = 0;
-
-		while (num != 5) {
-			System.out.println("================================");
-			System.out.println("Studycafe Management Menu");
-			System.out.println("================================");
-			System.out.println("1. Add Members");
-			System.out.println("2. Delete Members");
-			System.out.println("3. Edit Members");
-			System.out.println("4. View Members");
-			System.out.println("5. Exit");
-			System.out.println("Selete one number between 1-6 :");
-			num = input.nextInt();
-			
-			switch(num) {
-			case 1:
-				membermanager.addmembers();
-				break;
-			case 2:
-				membermanager.deletemembers();
-				break;
-			case 3:
-				membermanager.editmembers();
-				break;
-			case 4:
-				membermanager.viewmembers();
-				break;			
-			}
-			
-		}
 		
+		selectMenu(input,membermanager);
+	}
+	
+	public static void selectMenu(Scanner input, MemberManager membermanager ) {
+		int num = -1;
+		while (num != 5) {
+			try {
+				showMenu();
+				num = input.nextInt();
+				
+				switch(num) {
+				case 1:
+					membermanager.addmembers();
+					break;
+				case 2:
+					membermanager.deletemembers();
+					break;
+				case 3:
+					membermanager.editmembers();
+					break;
+				case 4:
+					membermanager.viewmembers();
+					break;			
+				}
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1and 5!");
+				if(input.hasNext()) {
+					input.next();
+				}
+				num = -1;
+				
+			}
+		}
+	}
+	public static void showMenu() {
+		System.out.println("================================");
+		System.out.println("Studycafe Management Menu");
+		System.out.println("================================");
+		System.out.println("1. Add Members");
+		System.out.println("2. Delete Members");
+		System.out.println("3. Edit Members");
+		System.out.println("4. View Members");
+		System.out.println("5. Exit");
+		System.out.println("Selete one number between 1-6 :");
 	}
 	
 }
